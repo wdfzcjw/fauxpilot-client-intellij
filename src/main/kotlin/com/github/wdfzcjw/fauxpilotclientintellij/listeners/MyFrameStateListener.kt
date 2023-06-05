25777983513df38c39ada205ef1883a426b5bebf
GitHub Action <action@github.com>
2023-06-05 02:11:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/wdfzcjw/fauxpilotclientintellij/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/wdfzcjw/fauxpilotclientintellij/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wdfzcjw.fauxpilotclientintellij.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
